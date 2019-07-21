d3f63457a68913334ea2d1e9a86bb599cd84f156
erik <czh198928@163.com>
2019-07-21 17:29:09 +0800

    “定义常量，并加载全局函数”

--- framework/start.php
+++ framework/start.php
@@ -3,13 +3,20 @@
 use framework\lib\light\App as AppPHP;
 
 //载入框架运行文件
-define('LIGHTPHP_PATH', realpath('./') );
-// define('LIB',LIGHTPHP_PATH.'../framework/lib');
-// define('LIGHT', LIB.'/light');
+define('LIGHTPHP_PATH', realpath('./') ); //项目执行当前目录
+define('LIGHTPHP', realpath('./../'));  //根目录
+//通过变量动态设置 module 的名字
+$MODULE_NAME = 'app';
+define('MODULE', $MODULE_NAME);
+define('APP', LIGHTPHP.'/'. MODULE .'/'); //模块目录
+define('FRAMEWORK', LIGHTPHP . '/framework/'); //核心文件目录
 
- include __DIR__. "/lib/light/Loader.php";
+define("DEBUG", getenv('APP_DEBUG'));
 
- spl_autoload_register('\Loader::load');
+include FRAMEWORK . 'lib/function/function.php';
+include FRAMEWORK . 'lib/light/Loader.php';
+
+spl_autoload_register('\Loader::load');
 
  //运行框架
  AppPHP::run();
